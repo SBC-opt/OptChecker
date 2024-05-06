2126b567d2cd4c001bf67984eb2fddf2c1a53e18
ZY546 <2358413808@qq.com>
2024-05-06 17:37:10 +0800

    copy file for reduce

---
+++ code/reduce.sh
@@ -0,0 +1,8 @@
+num=$1
+dir=$2
+
+cp -r $dir/Results_24_4_30/$num $dir/reduce/$num
+mkdir $dir/reduce/$num/code
+cp $dir/reduce/$num/test.cpp $dir/reduce/$num/code/test.cpp
+cp $dir/reduce/$num/init.h $dir/reduce/$num/code/init.h
+cp $dir/reduce/run_reduce_arithmetic.sh $dir/reduce/$num/code/run_reduce_arithmetic.sh
